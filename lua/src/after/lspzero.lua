8e6e8555315078729382bec43d59941aa11dce36
Calastian <christianwyattnovgrod@gmail.com>
2025-09-26 13:19:38 -0400

    added a ton of suppport for different languages

--- lua/src/after/lspzero.lua
+++ lua/src/after/lspzero.lua
@@ -1,11 +1,17 @@
 local lsp_zero = require('lsp-zero')
 
+-- lsp_zero.preset("recommended")
+
 lsp_zero.on_attach(function(client, bufnr)
     lsp_zero.default_keymaps({buffer = bufnr})
 end)
 
+-- Setup lsp-zero with modern vim.lsp.config
+lsp_zero.extend_lspconfig()
+
 -- Auto Complete:
 local cmp = require('cmp')
+local cmp_lsp = require("cmp_nvim_lsp")
 
 cmp.setup({
     sources = {
@@ -38,27 +44,48 @@ cmp.setup({
     },
 })
 
--- LSP Language Configuration:
+-- LSP Language Configuration with modern vim.lsp.config API:
+local capabilities = cmp_lsp.default_capabilities()
 
--- Lua
-require("lspconfig").lua_ls.setup({
+-- System Languages
+vim.lsp.config.lua_ls = {
+    cmd = { 'lua-language-server' },
+    capabilities = capabilities,
     settings = {
-        diagnostics = {
-            globals = { 'vim' },
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' },
+            },
         },
     }
-})
+}
 
--- C/C++
-require('lspconfig').clangd.setup({})
-require('lspconfig').cmake.setup({
-    filetypes = { "cmake", "CMakeLists.txt" }
-})
+vim.lsp.config.clangd = {
+    cmd = { 'clangd' },
+    capabilities = capabilities,
+}
 
-require('lspconfig').pylsp.setup({})
+vim.lsp.config.cmake = {
+    cmd = { 'cmake-language-server' },
+    capabilities = capabilities,
+    filetypes = { "cmake" }
+}
 
--- GoLang:
-require('lspconfig').gopls.setup({
+vim.lsp.config.rust_analyzer = {
+    cmd = { 'rust-analyzer' },
+    capabilities = capabilities,
+    settings = {
+        ['rust-analyzer'] = {
+            checkOnSave = {
+                command = 'clippy',
+            },
+        },
+    }
+}
+
+vim.lsp.config.gopls = {
+    cmd = { 'gopls' },
+    capabilities = capabilities,
     settings = {
         gopls = {
             analyses = {
@@ -68,12 +95,248 @@ require('lspconfig').gopls.setup({
             gofumpt = true,
         },
     },
-})
+}
 
--- Haskell:
-require('lspconfig').hls.setup({})
+vim.lsp.config.pylsp = {
+    cmd = { 'pylsp' },
+    capabilities = capabilities,
+}
 
--- JSON:
-require('lspconfig').jsonls.setup({
+vim.lsp.config.jdtls = {
+    cmd = { 'jdtls' },
+    capabilities = capabilities,
+}
+
+vim.lsp.config.omnisharp = {
+    cmd = { 'omnisharp' },
+    capabilities = capabilities,
+}
+
+-- Web Development
+vim.lsp.config.ts_ls = {
+    cmd = { 'typescript-language-server', '--stdio' },
+    capabilities = capabilities,
+    filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" }
+}
+
+vim.lsp.config.html = {
+    cmd = { 'vscode-html-language-server', '--stdio' },
+    capabilities = capabilities,
+}
+
+vim.lsp.config.cssls = {
+    cmd = { 'vscode-css-language-server', '--stdio' },
+    capabilities = capabilities,
+}
+
+vim.lsp.config.tailwindcss = {
+    cmd = { 'tailwindcss-language-server', '--stdio' },
+    capabilities = capabilities,
+}
+
+vim.lsp.config.emmet_ls = {
+    cmd = { 'emmet-ls', '--stdio' },
+    capabilities = capabilities,
+    filetypes = { "html", "css", "scss", "javascriptreact", "typescriptreact", "vue", "svelte" }
+}
+
+vim.lsp.config.vue_ls = {
+    cmd = { 'vue-language-server', '--stdio' },
+    capabilities = capabilities,
+}
+
+vim.lsp.config.svelte = {
+    cmd = { 'svelteserver', '--stdio' },
+    capabilities = capabilities,
+}
+
+vim.lsp.config.astro = {
+    cmd = { 'astro-ls', '--stdio' },
+    capabilities = capabilities,
+}
+
+-- Data & Config
+vim.lsp.config.jsonls = {
+    cmd = { 'vscode-json-language-server', '--stdio' },
+    capabilities = capabilities,
     filetypes = { "json", "mcmeta" }
+}
+
+vim.lsp.config.yamlls = {
+    cmd = { 'yaml-language-server', '--stdio' },
+    capabilities = capabilities,
+}
+
+vim.lsp.config.taplo = {
+    cmd = { 'taplo', 'lsp', 'stdio' },
+    capabilities = capabilities,
+}
+
+vim.lsp.config.lemminx = {
+    cmd = { 'lemminx' },
+    capabilities = capabilities,
+}
+
+-- Markup & Documentation
+vim.lsp.config.marksman = {
+    cmd = { 'marksman', 'server' },
+    capabilities = capabilities,
+}
+
+vim.lsp.config.ltex = {
+    cmd = { 'ltex-ls' },
+    capabilities = capabilities,
+    settings = {
+        ltex = {
+            language = "en-US",
+        },
+    }
+}
+
+-- Shell & DevOps
+vim.lsp.config.bashls = {
+    cmd = { 'bash-language-server', 'start' },
+    capabilities = capabilities,
+}
+
+vim.lsp.config.powershell_es = {
+    cmd = { 'pwsh', '-NoLogo', '-NoProfile', '-Command', 'PowerShellEditorServices' },
+    capabilities = capabilities,
+}
+
+vim.lsp.config.dockerls = {
+    cmd = { 'docker-langserver', '--stdio' },
+    capabilities = capabilities,
+}
+
+vim.lsp.config.docker_compose_language_service = {
+    cmd = { 'docker-compose-langserver', '--stdio' },
+    capabilities = capabilities,
+}
+
+vim.lsp.config.terraform_ls = {
+    cmd = { 'terraform-ls', 'serve' },
+    capabilities = capabilities,
+}
+
+vim.lsp.config.helm_ls = {
+    cmd = { 'helm_ls', 'serve' },
+    capabilities = capabilities,
+}
+
+-- Database
+vim.lsp.config.sqlls = {
+    cmd = { 'sql-language-server', 'up', '--method', 'stdio' },
+    capabilities = capabilities,
+}
+
+-- Other Popular Languages
+vim.lsp.config.phpactor = {
+    cmd = { 'phpactor', 'language-server' },
+    capabilities = capabilities,
+}
+
+vim.lsp.config.ruby_lsp = {
+    cmd = { 'ruby-lsp' },
+    capabilities = capabilities,
+}
+
+vim.lsp.config.kotlin_language_server = {
+    cmd = { 'kotlin-language-server' },
+    capabilities = capabilities,
+}
+
+vim.lsp.config.sourcekit = {
+    cmd = { 'sourcekit-lsp' },
+    capabilities = capabilities,
+}
+
+vim.lsp.config.r_language_server = {
+    cmd = { 'R', '--slave', '-e', 'languageserver::run()' },
+    capabilities = capabilities,
+}
+
+vim.lsp.config.julials = {
+    cmd = { 'julia', '--startup-file=no', '--history-file=no', '-e', 'using LanguageServer; runserver()' },
+    capabilities = capabilities,
+}
+
+vim.lsp.config.elixirls = {
+    cmd = { 'elixir-ls' },
+    capabilities = capabilities,
+}
+
+vim.lsp.config.erlangls = {
+    cmd = { 'erlang_ls' },
+    capabilities = capabilities,
+}
+
+vim.lsp.config.zls = {
+    cmd = { 'zls' },
+    capabilities = capabilities,
+}
+
+vim.lsp.config.nim_langserver = {
+    cmd = { 'nimlsp' },
+    capabilities = capabilities,
+}
+
+vim.lsp.config.crystalline = {
+    cmd = { 'crystalline' },
+    capabilities = capabilities,
+}
+
+vim.lsp.config.dartls = {
+    cmd = { 'dart', 'language-server', '--protocol=lsp' },
+    capabilities = capabilities,
+}
+
+vim.lsp.config.metals = {
+    cmd = { 'metals' },
+    capabilities = capabilities,
+}
+
+vim.lsp.config.fsautocomplete = {
+    cmd = { 'fsautocomplete', '--background-service-enabled' },
+    capabilities = capabilities,
+}
+
+vim.lsp.config.ocamllsp = {
+    cmd = { 'ocamllsp' },
+    capabilities = capabilities,
+}
+
+vim.lsp.config.hls = {
+    cmd = { 'haskell-language-server-wrapper', '--lsp' },
+    capabilities = capabilities,
+}
+
+vim.lsp.config.asm_lsp = {
+    cmd = { 'asm-lsp' },
+    capabilities = capabilities,
+}
+
+vim.lsp.config.nginx_language_server = {
+    cmd = { 'nginx-language-server' },
+    capabilities = capabilities,
+}
+
+-- Enable all configured language servers
+vim.lsp.enable({ 
+    -- System Languages
+    'lua_ls', 'clangd', 'cmake', 'rust_analyzer', 'gopls', 'pylsp', 'jdtls', 'omnisharp',
+    -- Web Development  
+    'ts_ls', 'html', 'cssls', 'tailwindcss', 'emmet_ls', 'vue_ls', 'svelte', 'astro',
+    -- Data & Config
+    'jsonls', 'yamlls', 'taplo', 'lemminx',
+    -- Markup & Documentation
+    'marksman', 'ltex',
+    -- Shell & DevOps
+    'bashls', 'powershell_es', 'dockerls', 'docker_compose_language_service', 'terraform_ls', 'helm_ls',
+    -- Database
+    'sqlls',
+    -- Other Languages
+    'phpactor', 'ruby_lsp', 'kotlin_language_server', 'sourcekit', 'r_language_server', 'julials',
+    'elixirls', 'erlangls', 'zls', 'nim_langserver', 'crystalline', 'dartls', 'metals', 
+    'fsautocomplete', 'ocamllsp', 'hls', 'asm_lsp', 'nginx_language_server'
 })
--- lua/src/after/mason.lua
+++ lua/src/after/mason.lua
@@ -1,9 +1,70 @@
 require("mason").setup()
 require('mason-lspconfig').setup({
-    ensure_installed = {'lua_ls' },
-    handlers = {
-        function(server_name)
-            require('lspconfig')[server_name].setup({})
-        end,
+    ensure_installed = { 
+        -- System Languages
+        'lua_ls',           -- Lua
+        'clangd',           -- C/C++
+        'cmake',            -- CMake
+        'rust_analyzer',    -- Rust
+        'gopls',            -- Go
+        'pylsp',            -- Python
+        'jdtls',            -- Java
+        'omnisharp',        -- C#
+        
+        -- Web Development
+        'ts_ls',            -- TypeScript/JavaScript
+        'html',             -- HTML
+        'cssls',            -- CSS
+        'tailwindcss',      -- Tailwind CSS
+        'emmet_ls',         -- Emmet
+        'vue_ls',           -- Vue.js
+        'svelte',           -- Svelte
+        'astro',            -- Astro
+        
+        -- Data & Config
+        'jsonls',           -- JSON
+        'yamlls',           -- YAML
+        'taplo',            -- TOML
+        'lemminx',          -- XML
+        
+        -- Markup & Documentation
+        'marksman',         -- Markdown
+        'ltex',             -- LaTeX/Grammar
+        
+        -- Shell & DevOps
+        'bashls',           -- Bash
+        'powershell_es',    -- PowerShell
+        'dockerls',         -- Dockerfile
+        'docker_compose_language_service', -- Docker Compose
+        'terraform_ls',     -- Terraform
+        'helm_ls',          -- Helm
+        
+        -- Database
+        'sqlls',            -- SQL
+        
+        -- Other Popular Languages
+        'phpactor',         -- PHP
+        'ruby_lsp',         -- Ruby
+        'kotlin_language_server', -- Kotlin
+        'sourcekit',        -- Swift
+        'r_language_server', -- R
+        'julials',          -- Julia
+        'elixirls',         -- Elixir
+        'erlangls',         -- Erlang
+        'zls',              -- Zig
+        'nim_langserver',   -- Nim
+        'crystalline',      -- Crystal
+        'dartls',           -- Dart
+        'metals',           -- Scala
+        'fsautocomplete',   -- F#
+        'ocamllsp',         -- OCaml
+        'hls',              -- Haskell (if it works in your environment)
+        
+        -- Assembly & Low Level
+        'asm_lsp',          -- Assembly
+        
+        -- Configuration Languages
+        'nginx_language_server', -- Nginx
     },
+    automatic_installation = true,
 })
